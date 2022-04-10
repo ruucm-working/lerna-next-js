f834d39472a4574b554f2a89949328b409ee3817
ruucm <ruucm.a@gmail.com>
2022-04-10 21:58:53 +0900

    framer-package

---
+++ packages/framer-package/src/index.tsx
@@ -0,0 +1,5 @@
+export { Button } from "./Button";
+export { MotionButton } from "./MotionButton";
+
+// export * from './components/atoms/Button'
+// export {Button2} from './components/atoms/Button2'
