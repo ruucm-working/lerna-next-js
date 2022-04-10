d069ea3581d31e392bf2628f930144ad4bd062a5
ruucm <ruucm.a@gmail.com>
2022-04-10 23:44:25 +0900

    ThreeBox

--- packages/shared/src/index.tsx
+++ packages/shared/src/index.tsx
@@ -3,5 +3,10 @@ export { MotionButton } from "./MotionButton";
 
 export { Icon } from './Icon'
 
+
+export { ThreeBox } from './ThreeBox'
+export { FiberCanvas, FiberContextProvider } from './useFiber'
+
+
 // export * from './components/atoms/Button'
 // export {Button2} from './components/atoms/Button2'
